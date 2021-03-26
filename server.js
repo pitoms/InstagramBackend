677ee3a08e545425e941f08a682aa582de3463b1
Pitom <pitoms@gmail.com>
2021-03-25 20:46:55 -0400

    adds basic routes and their descs as responses. clear devTests.html

--- server.js
+++ server.js
@@ -7,6 +7,21 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running at: http://localhost:${PORT}`));
 
 app.get("/", (req, res) => {
-  res.sendFile("devLogin.html");
-  console.log("main page hit");
+  res.send("Here are all fetched images in chronological order");
+});
+
+app.post("/createUser", (req, res) => {
+  res.send("User created with req body data.");
+});
+
+app.post("/addPhoto", (req, res) => {
+  res.send("Photo added to logged in users photos.");
+});
+
+app.get("/:username", (req, res) => {
+  res.send('Here is HTML of "usernames" profile.');
+});
+
+app.post("/deletePhoto/:photoid", (req, res) => {
+  res.send("Deleted photo from logged in user.");
 });
