19fd526242a02242880664448cf7127bbc41129a
Pitom <pitoms@gmail.com>
2021-03-28 16:42:17 -0400

    removes desc column from photos table from migrations

--- migrations/20210327022546_create_photos.js
+++ migrations/20210327022546_create_photos.js
@@ -2,7 +2,6 @@ exports.up = function (knex) {
   return knex.schema.createTable("photos", (table) => {
     table.increments("id");
     table.string("src");
-    table.string("desc");
     table.timestamp("time_posted").defaultTo(knex.fn.now());
     table.integer("user_id").unsigned().references("users.id");
   });
