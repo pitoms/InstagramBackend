ec895e932ae63de177836aef8b3a4883fe7e2d4b
Luis <vidals824@yahoo.com>
2021-03-29 01:26:55 -0400

    Added route to get users profile, made name in users a unique key

--- migrations/20210327022540_create_users.js
+++ migrations/20210327022540_create_users.js
@@ -1,7 +1,7 @@
 exports.up = function (knex) {
   return knex.schema.createTable("users", (table) => {
     table.increments("id");
-    table.string("name");
+    table.string("name").unique();
     table.string("email");
     table.string("pswd_hash");
   });
